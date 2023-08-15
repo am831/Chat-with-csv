cee6d366594d02506c5d0b21a5c31b5501e6320e
Alisha Maddy <alisha831maddy@gmail.com>
2023-08-15 15:05:43 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Chat with kindle", page_icon=":smiley:")
+
+with st.container():
+    st.subheader("Hi, I am Alisha. I am here to help you with your kindle related queries.")
+    st.title("Chat with kindle")
+    st.write("Please enter your query below and I will try to answer it for you.")
+
+with st.container():
+    st.write("---")
+    
