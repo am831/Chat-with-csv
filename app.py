1f60492af01c3e19bc8cf9724d32ce7d00dbf05d
Alisha Maddy <alisha831maddy@gmail.com>
2023-08-16 08:11:52 -0700

    everything works

--- app.py
+++ app.py
@@ -1,12 +1,61 @@
 import streamlit as st
+from query_data import create_gp_instance, create_oai_instance, query_ai
+import asyncio
+import pandas as pd
+from PIL import Image
+import base64
+from io import BytesIO
 
-st.set_page_config(page_title="Chat with kindle", page_icon=":smiley:")
+image_path = "./static/csvicon.png"
+image = Image.open(image_path)
+image_bytes = BytesIO()
+image.save(image_bytes, format='PNG')
+image_base64 = base64.b64encode(image_bytes.getvalue()).decode()
 
-with st.container():
-    st.subheader("Hi, I am Alisha. I am here to help you with your kindle related queries.")
-    st.title("Chat with kindle")
-    st.write("Please enter your query below and I will try to answer it for you.")
+# Create the Markdown with embedded base64 image
+markdown = f"""
+<div style="display: flex; align-items: center;">
+    <img src="data:image/png;base64,{image_base64}" style="width: 60px;">
+    <h1 style="margin-right: 10px;">Query Your CSV With Natural Language</h1>
+</div>
+"""
+st.markdown(markdown, unsafe_allow_html=True)
 
-with st.container():
-    st.write("---")
-    
+global_vars = {
+    "api_key": None
+}
+
+assistant_creation_functions = [
+    ("Chat-GPT", create_oai_instance),
+    ("Google PaLM", create_gp_instance)
+]
+
+async def main():
+    st.write("Choose an assistant:")
+    selected_choice = st.selectbox("", [""] + [assistant[0] for assistant in assistant_creation_functions])
+
+    if selected_choice:
+        st.write("Please enter your API key:")
+        global_vars["api_key"] = st.text_input("API Key")
+
+        if global_vars["api_key"]:
+            kernel, assistant = None, None
+            for assistant_name, create_func in assistant_creation_functions:
+                if selected_choice == assistant_name:
+                    kernel, assistant = create_func(api_key=global_vars["api_key"])
+                    break  # Exit the loop once the correct assistant is found
+            
+            if kernel and assistant:
+                st.write("Please upload your CSV file below.")
+                data = st.file_uploader("Upload a CSV")
+                query = st.text_area("Insert your query")
+
+                if st.button("Submit Query", type="primary") and data is not None:
+                    df = pd.read_csv(data)
+                    response = await query_ai(kernel=kernel, assistant=assistant, query=query, data=df)
+                    st.write(response)
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
---
+++ query_data.py
@@ -0,0 +1,28 @@
+import semantic_kernel as sk
+import pandas as pd
+from semantic_kernel.core_skills import DataSkill
+import semantic_kernel.connectors.ai.google_palm as sk_gp
+import semantic_kernel.connectors.ai.open_ai as sk_oai
+
+def create_gp_instance(api_key):
+    kernel = sk.Kernel()
+    palm_chat_completion = sk_gp.GooglePalmChatCompletion(
+        "models/chat-bison-001", api_key
+    )
+    kernel.add_chat_service("models/chat-bison-001", palm_chat_completion)
+    return kernel, palm_chat_completion
+
+def create_oai_instance(api_key):
+    kernel = sk.Kernel()
+    openai_chat_completion = sk_oai.OpenAIChatCompletion("gpt-3.5-turbo", api_key)
+    kernel.add_chat_service("chat_service", openai_chat_completion)
+    return kernel, openai_chat_completion
+
+
+async def query_ai(kernel, assistant, query: str, data: pd.DataFrame):
+    data_skill = kernel.import_skill(
+        DataSkill(data=data, service=assistant), skill_name="data"
+    )
+    query_async = data_skill["queryAsync"]
+    result = await query_async.invoke_async(query)
+    return result
